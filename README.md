2e72b7460a3fd404cd2fdbf063166eb9f0ea6faf
Leecz888 <leecz888@hotmail.com>
2024-04-09 22:22:28 +0800

    Added audio conversion to spectrogram and standardised audio samples

---
+++ README.md
@@ -0,0 +1,3 @@
+# 50.039 Deep Learning Project
+
+Before running the code, it is recommended to setup a conda virtual environment
--- model.py
+++ model.py
@@ -1,12 +1,13 @@
+import json
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 import pandas as pd
 
 class CovidCoughDataset(Dataset):
     def __init__(self, filename):
-        self.data = pd.read_csv(filename)
-        self.inputs = self.data["uuid"]
-        self.outputs = self.data["status"]
+        self.data = pd.read_excel(filename)
+        self.inputs = self.data["Filename"]
+        self.outputs = self.data["Status"]
 
     def __getitem__(self, index):
         x = self.inputs.iloc[index]
@@ -17,5 +18,17 @@ class CovidCoughDataset(Dataset):
         return len(self.data)
     
 if __name__ == '__main__':
-    print("Hello World")
-         
+    
+    with open("./model.conf", mode="r") as conf:
+        config = json.loads(conf.read())
+    
+    batch_size = config["batch_size"]
+    
+    dataset = CovidCoughDataset('./Data/Dataset.xlsx')
+    train_dataset, valid_dataset, test_dataset = random_split(dataset, [0.8, 0.1, 0.1])
+    train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size = batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle=True)
+    
+    
+    
