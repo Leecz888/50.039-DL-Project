c7eef1bbbbe9aa94c569dfd01072e1c81b36ef75
cleavecamael <phoenixlee125@gmail.com>
2024-04-14 22:42:49 +0800

    Updated With new format

--- noise_reduction.py
+++ noise_reduction.py
@@ -4,12 +4,11 @@ import os
 # load data
 import os.path
 
-
+#Reduce Noise in Audio Data
 def noise_reduce(input_folder, output_folder):
     i = 0
     output_files = os.listdir(output_folder)
     for filename in os.listdir(input_folder):
-        print(f"Number {i}")
         i += 1
         if f'{output_folder}/{filename}' in output_files:
             return
@@ -23,6 +22,3 @@ def noise_reduce_file(input_folder,  output_folder,  filename,):
     # perform noise reduction
     reduced_noise = nr.reduce_noise(y=data, sr=rate)
     wavfile.write(f'{output_folder}/{filename}', rate, reduced_noise)
-if __name__ == "__main__":
-    noise_reduce_file("./Input", "./noisereduced", "0e56d89b-3d5c-433e-a93e-bf8c6fd998f8.wav")
-    #     convert_audio(f'./Data/Covid/{filename}', f'./Converted/{filename}')
--- silence.py
+++ silence.py
@@ -3,16 +3,14 @@ import os
 from pydub import AudioSegment, silence
 # load data
 def trim_silence(input_folder, output_folder):
-    i = 0
     for filename in os.listdir(input_folder):
-        if filename in os.listdir(output_folder):
-            print(i)
-            i+= 1
-            continue
         song = AudioSegment.from_wav(f"{input_folder}/{filename}")
+        #If error loading file, skip
         if not song:
             continue
+        #Find all non silent regions
         regions = silence.detect_nonsilent(song, silence_thresh = -100)
+        #Identify the loudest non-silent region
         if regions:
             max_volume = -99999
             max_split = None
@@ -28,16 +26,5 @@ def trim_silence(input_folder, output_folder):
             max_split = song
         if max_split == None:
             continue
+        #Export loudest non-silent region
         max_split.export(f"{output_folder}/{filename}", format="wav")
-        print(i)
-        i += 1
-if __name__ == "__main__":
-    trim_silence("./tmp2", "./tmp1")
-
-    #rate, data = wavfile.read(f"./converted/{filename}")
-    # perform noise reduction
-    #reduced_noise = nr.reduce_noise(y=data, sr=rate)
-    #wavfile.write(f'./test/{filename}', rate, reduced_noise)
-    #print(f"Number {i}")
-    #i += 1
-#     convert_audio(f'./Data/Covid/{filename}', f'./nverted/{filename}')
